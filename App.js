dd711b2e2ffe85f3b32040e8a2840e5fbf5ecfaa
im4th <im4thone@gmail.com>
2023-10-15 17:52:45 +0200

    section 2 - up to lesson 22

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {useState} from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Siemaaano</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [enteredGoalText, setEnteredGoalText] = useState('')
+
+    function goalInputHandler(enteredText) {
+        setEnteredGoalText(enteredText);
+    }
+
+    function addGoalHandler() {
+        console.log(enteredGoalText)
+    }
+
+    return (<View style={styles.appContainer}>
+        <View style={styles.inputContainer}>
+            <TextInput style={styles.textInput} placeholder="Your course goal!" onChangeText={goalInputHandler}/>
+            <Button title="Add goal" onPress={addGoalHandler}/>
+        </View>
+        <View style={styles.goalsContainer}>
+            <Text>List of goals</Text>
+        </View>
+    </View>);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    appContainer: ({
+        flex: 1, paddingTop: 50, paddingHorizontal: 16,
+    }), inputContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 24,
+        borderBottomWidth: 1,
+        borderColor: 'black',
+    }, textInput: {
+        borderWidth: 1, borderColor: '#cccccc', width: '70%', marginRight: 8, padding: 8
+    }, goalsContainer: {
+        flex: 5,
+    }
 });
