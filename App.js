1879d4aa24bf24743f30702dad7b86f5abc512d4
im4th <im4thone@gmail.com>
2023-10-20 14:28:16 +0200

    section 2 - up to lesson 26

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
-import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {Button, FlatList, StyleSheet, Text, TextInput, View} from 'react-native';
 import {useState} from 'react';
 
 export default function App() {
     const [enteredGoalText, setEnteredGoalText] = useState('')
+    const [courseGoals, setCourseGoals] = useState([]);
 
     function goalInputHandler(enteredText) {
         setEnteredGoalText(enteredText);
     }
 
     function addGoalHandler() {
-        console.log(enteredGoalText)
+        setCourseGoals(currentCourseGoals => [...currentCourseGoals, {text: enteredGoalText, id: Math.random().toString()},]);
     }
 
     return (<View style={styles.appContainer}>
@@ -18,7 +19,11 @@ export default function App() {
             <Button title="Add goal" onPress={addGoalHandler}/>
         </View>
         <View style={styles.goalsContainer}>
-            <Text>List of goals</Text>
+            <FlatList data={courseGoals} renderItem={itemData => {
+                return (<View style={styles.goalItem}>
+                    <Text style={styles.goalText}>{itemData.item.text}</Text>
+                </View>)
+            }} keyExtractor={(item,index) => {return item.id;}}/>
         </View>
     </View>);
 }
@@ -38,5 +43,9 @@ const styles = StyleSheet.create({
         borderWidth: 1, borderColor: '#cccccc', width: '70%', marginRight: 8, padding: 8
     }, goalsContainer: {
         flex: 5,
+    }, goalItem: {
+        margin: 8, borderRadius: 6, backgroundColor: "#5e0acc", padding: 8
+    }, goalText: {
+        color: "white"
     }
 });
