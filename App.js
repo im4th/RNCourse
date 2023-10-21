80203c4bda24c4969fe004f9dd374f76eeeeaaf7
im4th <im4thone@gmail.com>
2023-10-21 17:18:14 +0200

    End of Section 2

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import {Button, FlatList, StyleSheet, Text, TextInput, View} from 'react-native'
 import {useState} from 'react';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
     const [modalVisible,setModalVisibility] = useState(false);
@@ -26,19 +27,22 @@ export default function App() {
         setModalVisibility(false);
     }
 
-    return (<View style={styles.appContainer}>
-    <Button title="Add New Goal" color={"#5e0acc"} onPress={startAddGoalHandler}></Button>
+    return (
+    <>
+    <View style={styles.appContainer}>
+    <Button title="Add New Guest" color={"#5e0acc"} onPress={startAddGoalHandler}></Button>
         <GoalInput onCancel={endAddGoalHandler} visible = {modalVisible} onAddGoal = {addGoalHandler}/>
         <View style={styles.goalsContainer}>
             <FlatList data={courseGoals} renderItem={itemData => {
                 return (<GoalItem text={itemData.item.text} id={itemData.item.id} onDeleteItem={deleteGoalHandler}/>)
             }} keyExtractor={(item,index) => {return item.id;}}/>
         </View>
-    </View>);
+    </View>
+    </>);
 }
 const styles = StyleSheet.create({
     appContainer: ({
-        flex: 1, paddingTop: 50, paddingHorizontal: 16,
+        flex: 1, paddingTop: 50, paddingHorizontal: 16, backgroundColor: "#1e085a"
     }),goalsContainer: {
         flex: 5,
     }
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -17,12 +17,13 @@ function GoalInput(props){
                 <View style={styles.inputContainer}>
                 <Image style={styles.image} source={require("../assets/images/watermark.jpg")}/>
                     <TextInput style={styles.textInput} 
-                        placeholder="Your guest name" 
+                        placeholder="Your guest name:"
+                        // placeholderTextColor={"white"}
                         onChangeText={goalInputHandler} z
                         value={enteredGoalText}/>
                         <View style={styles.buttonContainer}>
-                        <View style= {styles.button}><Button title="Add guest" onPress={addGoalHandler}/></View>
-                        <View style= {styles.button}><Button title="cancel" onPress={props.onCancel}></Button></View>
+                        <View style= {styles.button}><Button title="Add guest" onPress={addGoalHandler} color={"#5e0acc"}/></View>
+                        <View style= {styles.button}><Button title="cancel" onPress={props.onCancel} color={"#f31282"}></Button></View>
                         </View>
                 </View>
             </Modal>);
@@ -32,7 +33,14 @@ export default GoalInput;
 
 const styles = StyleSheet.create({
     textInput: {
-        borderWidth: 1, borderColor: '#cccccc', width: '100%', marginRight: 8, padding: 8
+        borderWidth: 1,
+        borderColor: '#e4d0ff',
+        backgroundColor: '#e4d0ff',
+        borderRadius: 6,
+        width: '100%',
+        marginRight: 8,
+        padding: 8,
+        color: "#120438",
     }, inputContainer: {
         flex: 1,
         justifyContent: 'center',
